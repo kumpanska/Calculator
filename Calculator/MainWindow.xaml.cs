b338ce34020cf297a602b6ab8990056c9da15af5
Kate Kumpanska <kate.kumpansky@gmail.com>
2025-03-16 15:39:12 +0200

    add change in design and add methods for input and divide

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,22 @@ namespace Calculator
         public MainWindow()
         {
             InitializeComponent();
+        }
+        double first;
+        double second;
+        char operation;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            Result.Text += btn.Content.ToString();
+            second = Convert.ToDouble(Result.Text);
+        }
+
+        private void DivideButton_Click(object sender, RoutedEventArgs e)
+        {
+            first = Convert.ToDouble(Result.Text);
+            operation = '÷';
+            Result.Clear();
         }
     }
 }
